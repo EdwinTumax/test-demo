2366f5088cb10f08168cd3d281ef0ec26a5783e1
Edwin Tumax <edwintumax@gmail.com>
2021-12-02 19:44:48 -0600

    Primera version

---
+++ Sistema.java
@@ -0,0 +1,11 @@
+package org.edwintumax.main;
+public class Sistema {
+    public static void main(String args[]){
+        int edad = 17;
+        if(edad > 18){
+            System.out.println("Es mayor de edad");
+        }else {
+            System.out.println("Es menor de edad");
+        }
+    }
+}
